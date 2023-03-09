3a7a3c5499e0b78e408e9cae5148539b1c943ada
reutbukra <80385346+reutbukra@users.noreply.github.com>
2023-03-10 00:33:19 +0200

    Update auth_route.ts

--- src/routes/auth_route.ts
+++ src/routes/auth_route.ts
@@ -1,14 +1,15 @@
 import express from 'express'
 const router = express.Router()
-import auth from '../controllers/auth'
+import auth from '../controllers/auth.js'
+
 /**
 * @swagger
 * components:
 *   securitySchemes:
-*       bearerAuth:
-*           type: http
-*           scheme: bearer
-*           bearerFormat: JWT
+*     bearerAuth:
+*       type: http
+*       scheme: bearer
+*       bearerFormat: JWT
 */
 
 /**
@@ -22,128 +23,195 @@ import auth from '../controllers/auth'
 * @swagger
 * components:
 *   schemas:
-*       User:
-*          type: object
-*          required:
-*            - email
-*            - password
-*          properties:
-*            email:
-*              type: string
-*              description: The user email
-*            password:
-*              type: string
-*              description: The user password
-*          example:
-*            email: 'bob@gmail.com'
-*            password: '123456'
-*/
-
-/**
-* @swagger
-* components:
-*   schemas:
-*     Tokens:
+*     User:
 *       type: object
 *       required:
-*         - accessToken
-*         - refreshToken
+*         - email
+*         - password
+*         - imageUrl
+*         - name
 *       properties:
-*         accessToken:
+*         email:
 *           type: string
-*           description: The JWT access token
-*         refreshToken:
+*           description: The user email
+*         password:
 *           type: string
-*           description: The JWT refresh token
+*           description: The user password
+*         name:
+*           type: string
+*           description: The user name
+*         image:
+*           type: string
+*           description: The user's profile picture url
 *       example:
-*         accessToken: '123cd123x1xx1'
-*         refreshToken: '134r2134cr1x3c'
+*         email: 'bob@gmail.com'
+*         password: '123456'
+*         name: 'Bob'
+*         image: ''
 */
 
-/**
-* @swagger
-* /auth/register:
-*   post:
-*     summary: registers a new user
-*     tags: [Auth]
-*     requestBody:
-*       required: true
-*       content:
-*         application/json:
-*           schema:
-*             $ref: '#/components/schemas/User'
-*     responses:
-*       200:
-*         description: Register success return user info
-*         content:
-*           application/json:
-*             schema:
-*               $ref: '#/components/schemas/User'
-*       400:
-*         description: Register error
-*         content:
-*           application/json:
-*             schema:
-*               err:
-*                 type: string
-*                 description: The error description
-*/
-router.post('/register',auth.register)
 
 /**
-* @swagger
-* /auth/login:
-*   post:
-*     summary: login a user
-*     tags: [Auth]
-*     requestBody:
-*       required: true
-*       content:
-*         application/json:
-*           schema:
-*             $ref: '#/components/schemas/User'
-*     responses:
-*       200:
-*         description: Login success return access and refresh tokens
-*         content:
-*           application/json:
-*             schema:
-*               $ref: '#/components/schemas/User'
-*/
-router.post('/login',auth.login)
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: registers a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: Register success retuns user info
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Registeration error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               err:
+ *                 type: string
+ *                 description: The error description 
+ *  
+ */
+router.post('/register', auth.register)
+
 
 /**
-* @swagger
-* /auth/refresh:
-*   get:
-*     summary: get a new access token using the refresh token
-*     tags: [Auth]
-*     description: need to provide the refresh token in the auth header
-*     security:
-*       - bearerAuth: []
-*     responses:
-*       200:
-*         description: The acess & refresh tokens
-*         content:
-*           application/json:
-*             schema:
-*               $ref: '#/components/schemas/Tokens'
-*/
-router.get('/refresh',auth.refresh)
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: login a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: Login success retuns access and refresh tokens
+ *         content:
+ *           application/json:
+ *             schema:
+ *               access_token:
+ *                 type: string
+ *                 description: The Access Token
+ *               refresh_token:
+ *                 type: string
+ *                 description: The Refresh Token
+ *             example:
+ *               access_token: '223412341...'
+ *               refresh_token: '123456...'
+ *
+ */
+router.post('/login', auth.login)
 
 /**
-* @swagger
-* /auth/logout:
-*   get:
-*     summary: logout a user
-*     tags: [Auth]
-*     description: need to provide the refresh token in the auth header
-*     security:
-*       - bearerAuth: []
-*     responses:
-*       200:
-*         description: Logout completed successfully
-*/
-router.get('/logout',auth.logout)
+ * @swagger
+ * /auth/logout:
+ *   get:
+ *     summary: logout user invalidate refresh token
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: logout sucess, refresh token is invalidated
+ *
+ */
+router.get('/logout', auth.logout)
+
+/**
+ * @swagger
+ * /auth/refresh:
+ *   get:
+ *     summary: refresh access token
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: refresh token success retuns access and refresh tokens
+ *         content:
+ *           application/json:
+ *             schema:
+ *               access_token:
+ *                 type: string
+ *                 description: The Access Token
+ *               refresh_token:
+ *                 type: string
+ *                 description: The Refresh Token
+ *             example:
+ *               access_token: '223412341...'
+ *               refresh_token: '123456...'
+ *
+ */
+router.get('/refresh', auth.refresh)
+
+
+
+
+/**
+ * @swagger
+ * /auth/{id}:
+ *   get:
+ *     summary: get user by id
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         requiered: true
+ *         schema:
+ *           type: string
+ *           description: the requested user id
+ *     responses:
+ *       200:
+ *         description: the requested user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *
+ */
+router.get('/:id', auth.authenticateMiddleware, auth.getUserById) 
+// TODO - need to add test for getUserById in the auth.test.ts
+
+
+
+/**
+ * @swagger
+ * /auth/{id}:
+ *   put:
+ *     summary: update user by id
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         requiered: true
+ *         schema:
+ *           type: string
+ *           description: the updated user id
+ *     responses:
+ *       200:
+ *         description: the updated user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *
+ */
+router.put("/:id", auth.authenticateMiddleware, auth.putUserById);
 
 export = router
