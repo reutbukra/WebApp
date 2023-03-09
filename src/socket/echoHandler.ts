c7855d3732a9cd87bc47af65db3a50cd81c16c40
reutbukra <80385346+reutbukra@users.noreply.github.com>
2023-03-10 00:38:26 +0200

    Update echoHandler.ts

--- src/socket/echoHandler.ts
+++ src/socket/echoHandler.ts
@@ -1,19 +1,20 @@
+
 import { Server, Socket } from "socket.io"
 import { DefaultEventsMap } from "socket.io/dist/typed-events"
 
-export = (io:Server<DefaultEventsMap,DefaultEventsMap,DefaultEventsMap>,
-         socket:Socket<DefaultEventsMap,DefaultEventsMap,DefaultEventsMap>) => {
+export = (io:Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap>, 
+            socket:Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap>) => {
 
     const echoHandler = (payload) => {
-        console.log("echo:echo")
+        console.log("echoHandler emit echo:echo_res ")
         socket.emit('echo:echo_res', payload)
     }
-
+ 
     const readHandler = (payload) => {
-        console.log("echo:read")
-        socket.emit('echo:echo_req', payload)
+        // ...
     }
-    console.log("register echo handler")
-    socket.on("echo:echo", echoHandler);
-    socket.on("echo:read", readHandler);
+    console.log('register echo handlers')
+    socket.on("echo:echo", echoHandler)
+    socket.on("echo:read", readHandler)
 }
+ 
